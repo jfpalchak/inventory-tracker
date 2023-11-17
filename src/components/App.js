8e9fcddf282acbbf35472d270e2603f95cd711e9
Joey Palchak <joey.palchak@gmail.com>
2023-11-17 08:35:53 -0800

    create Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React from 'react';
+import Header from './Header';
 
 function App() {
   return (
     <React.Fragment>
-
+      <Header/>
+      
     </React.Fragment>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Shop</a></li>
+        </ul>
+      </nav>
+
+      <div className="site-logo">
+        LOGO
+      </div>
+
+      <div className="cart-widget">
+        Cart
+      </div>
+    </header>
+  );
+}
+
+export default Header;
