86c0ae27a02a646587d6ec7eafa7994e70be6eb1
Joey Palchak <joey.palchak@gmail.com>
2023-11-17 08:52:26 -0800

    add test array of coffee beans

---
+++ src/components/test-list.js
@@ -0,0 +1,18 @@
+export const testData = [
+  {
+    name: "Cafe Saboroso",
+    origin: "Brazil",
+    roast: "Dark",
+    price: 13.40,
+    quantity: 130,
+    id: "1"
+  },
+  {
+    name: "Worka Sakaro",
+    origin: "Ethiopia",
+    roast: "Medium",
+    price: 21.00,
+    quantity: 130,
+    id: "2"
+  }
+];
